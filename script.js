e3250c1eee7b5407abec089d11c3c3bfde3cc074
JYOTI <70147432+jyoti21266@users.noreply.github.com>
2021-05-02 15:13:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let loadImage = ( src , callback) => {
 };
 
 let imagePath = (frameNumber, animation) => {
-    return "/images/" + animation + "/" + frameNumber + ".png";
+    return "../canvas game/finisher/images/" + animation + "/" + frameNumber + ".png";
 };
 
 let frames = {
